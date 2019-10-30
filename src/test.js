f7f61c606fdf845ce6d5292fef0604533a325410
Gemma Waylett <gwaylett@gmail.com>
2019-10-30 16:50:21 -0400

    adding chai

--- src/test.js
+++ src/test.js
@@ -3,7 +3,11 @@
 
     var assert = require("chai").assert;
 
+    // Basic addition
     assert.equal(add(3,4), 7);
+
+    // IEEE 754 float point
+    assert.equal(add(0.1, 0.2), 0.3)
 
     function add(a, b) {
             return a + b;
