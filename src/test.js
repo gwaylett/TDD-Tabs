8a6363af0dccbc6c256907653e0c9e89230b6851
Gemma Waylett <gwaylett@gmail.com>
2019-10-30 16:40:41 -0400

    chai and assertions

--- src/test.js
+++ src/test.js
@@ -1,6 +1,13 @@
 (function () {
     "use strict";
 
+    var assert = require("chai").assert;
+
+    assert.equal(add(3,4), 7);
+
+    function add(a, b) {
+            return a + b;
+        }
 
     // Try/Catch
     // var actual = add(3, 4);
@@ -14,17 +21,16 @@
     // }
 
 
-
-    
     // Basic Assertion Example
-    assertEqual(add(3,4), 7);
+    // assertEqual(add(3,4), 7);
 
-    function add(a, b) {
-        return a + b;
-    }
+    // function add(a, b) {
+    //     return a + b;
+    // }
 
-    function assertEqual(actual, expected){
-        if (actual !== expected) throw new Error("Expected: " + expected + ", but got " + actual);
-    }
+    // function assertEqual(actual, expected){
+    //     if (actual !== expected) throw new Error("Expected: " + expected + ", but got " + actual);
+    // }
+
 
 })();
