a48a348e7cbfab2f5715c83541ee61a3ca001472
Gemma Waylett <gwaylett@gmail.com>
2019-10-30 16:15:01 -0400

    Assertions

--- src/test.js
+++ src/test.js
@@ -1,3 +1,24 @@
-(function(){
+(function () {
     "use strict";
+
+    // var actual = add(3, 4);
+    // var expected = 7;
+
+    // try {
+    //    
+    // }
+    // catch (err) {
+    //     console.log("Caught exception: " + err);
+    // }
+
+    assertEqual(add(3,4), 7);
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    function assertEqual(actual, expected){
+        if (actual !== expected) throw new Error("Expected: " + expected + ", but got " + actual);
+    }
+
 })();
