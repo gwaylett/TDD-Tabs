fbbfa1cdccecf4c308f90d649292556f70a008fd
Gemma Waylett <gwaylett@gmail.com>
2019-10-30 16:15:59 -0400

    assertions

--- src/test.js
+++ src/test.js
@@ -1,16 +1,22 @@
 (function () {
     "use strict";
 
+
+    // Try/Catch
     // var actual = add(3, 4);
     // var expected = 7;
 
     // try {
-    //    
+    //    if (actual !== expected) throw new Error("Expected: " + expected + ", but got " + actual);
     // }
     // catch (err) {
     //     console.log("Caught exception: " + err);
     // }
 
+
+
+    
+    // Basic Assertion Example
     assertEqual(add(3,4), 7);
 
     function add(a, b) {
