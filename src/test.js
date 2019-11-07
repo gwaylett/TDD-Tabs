a4f4bc1afdd4f0b61b3590d26d5abc919554c556
Gemma Waylett <gwaylett@gmail.com>
2019-11-07 12:56:53 -0500

    adding karma

--- src/test.js
+++ src/test.js
@@ -14,10 +14,19 @@
         it("uses IEEE 754 Floating point", function () {
             assert.equal(add(0.1, 0.2), 0.30000000000000004);
         })
+
+        it("throws a message if numbers are negative", function(){
+            assert.equal(add(1, -5), "Sorry can't add negatives!")
+        })
     });
 
     function add(a, b) {
+        if ( a > 0 && b > 0) {
         return a + b;
+        }
+        else {
+            return "Sorry can't add negatives!";
+        }
     }
 
 
