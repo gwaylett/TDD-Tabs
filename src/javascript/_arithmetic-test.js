c0b03335e4feddf921afcce09f68a545334449f5
Gemma Waylett <gwaylett@gmail.com>
2019-11-13 15:02:52 -0500

    getting rid of old stuff

--- src/javascript/_arithmetic-test.js
+++ src/javascript/_arithmetic-test.js
@@ -2,42 +2,13 @@
 
     "use strict";
 
-    var arithmetic = require("./arithmetic.js");
     var assert = require("./assert.js");
 
-    describe("Addition", function () {
+    describe("Something", function () {
 
-        it("adds positive numbers", function () {
-            assert.equal(arithmetic.add(3, 4), 7);
-        });
-
-        it("uses IEEE 754 Floating point", function () {
-            assert.equal(arithmetic.add(0.1, 0.2), 0.30000000000000004);
-        });
-
-        it("throws a message if numbers are negative", function () {
-            assert.equal(arithmetic.add(1, -5), "Sorry can't add negatives!");
+        it("something", function () {
+           
         });
 
     });
-
-    describe("Subtraction", function () {
-        it("substracts two positive numbers", function () {
-            assert.equal(arithmetic.subtract(10, 3), 7);
-        });
-    });
-
-    describe("Multiplication", function () {
-        it("multiplies two numbers", function () {
-            assert.equal(arithmetic.multiply(4, 6), 24);
-        });
-    });
-
-
-    describe("Division", function () {
-        it("divides two positive numbers", function () {
-            assert.equal(arithmetic.divide(36, 6), 6);
-        });
-    });
-
 })();
--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,9 +2,5 @@
 
     "use strict";
 
-    var arithmetic = require("./arithmetic.js");
-
-    console.log("Hello!");
-    console.log("42 + 13 = " + arithmetic.add(42, 13));
 
 })();
