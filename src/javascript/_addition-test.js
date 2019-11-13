e34a6ef85ce75a9747104a5771cc96e253565755
Gemma Waylett <gwaylett@gmail.com>
2019-11-13 12:29:19 -0500

    installed chai for assertions

--- src/javascript/_addition-test.js
+++ src/javascript/_addition-test.js
@@ -3,24 +3,21 @@
     "use strict";
 
     var addition = require("./addition.js");
+    var assert = require("../vendor/chai-4.2.0.js").assert;
 
     describe("Addition", function () {
 
         it("adds positive numbers", function () {
-            assertEqual(addition.add(3, 4), 7);
+            assert.equal(addition.add(3, 4), 7);
         });
 
         it("uses IEEE 754 Floating point", function () {
-            assertEqual(addition.add(0.1, 0.2), 0.30000000000000004);
+            assert.equal(addition.add(0.1, 0.2), 0.30000000000000004);
         });
 
         it("throws a message if numbers are negative", function () {
-            assertEqual(addition.add(1, -5), "Sorry can't add negatives!");
+            assert.equal(addition.add(1, -5), "Sorry can't add negatives!");
         });
-
-        function assertEqual(actual, expected) {
-            if (actual !== expected) throw new Error("expected " + expected + ", but was " + actual);
-        }
 
     });
 })();
--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,7 +2,7 @@
 
     "use strict";
 
-    var addition = require("./addition");
+    var addition = require("./addition.js");
 
     console.log("Hello!");
     console.log("42 + 13 = " + addition.add(42, 13));
