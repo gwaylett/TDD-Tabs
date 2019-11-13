e13e2b7a1514cf18d478bf8e3882cb0eb2641596
Gemma Waylett <gwaylett@gmail.com>
2019-11-13 16:35:43 -0500

    created tab libaray

---
+++ src/javascript/_tabs-test.js
@@ -0,0 +1,23 @@
+(function () {
+
+    "use strict";
+
+    var assert = require("./assert.js");
+    var tabs = require("./tabs.js");
+
+    describe("Tabs", function () {
+
+        it("has an API", function () {        
+            tabs.initialize();
+
+
+            // var div = document.createElement("div");
+            // div.innerHTML = "this is an example.";
+            // document.body.appendChild(div);
+            // div.className = "test";
+            // div.appendChild(p);
+            //div.parentNode.removeChild(div);
+        });
+
+    });
+})();
