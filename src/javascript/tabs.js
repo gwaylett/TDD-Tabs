abdf9e1063be7ccab302455799685427d826bf92
Gemma Waylett <gwaylett@gmail.com>
2019-11-15 14:42:58 -0500

    refactored fail fast

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -8,30 +8,35 @@
         var activeTabClass = options.activeTabClass;
         var contentHideClass = options.contentHideClass;
 
+        checkOption(tabs, "options.tabs");
+        checkOption(content, "options.content");
+        checkOption(defaultElement, "options.defaultElement");
+        checkOption(activeTabClass, "options.activeTabClass");
+        checkOption(contentHideClass, "options.contentHideClass");
 
-        if (tabs === undefined) throw new Erorr ("Expected options.tab");
-        if (content === undefined) throw new Error ("Expected options.content");
-        if (defaultElement === undefined) throw new Error ("Expected options.default");
-        if (activeTabClass === undefined) throw new Error ("Expected options.activeTabClass");
-        if (contentHideClass === undefined) throw new Error ("Expected options.contentHideClass");
-
-        content.forEach(function(element) {
+        content.forEach(function (element) {
             element.classList.add(contentHideClass);
         });
 
         defaultElement.classList.remove(contentHideClass);
 
-        var activeIndex = findIndexOfDefaultElement(content, defaultElement);  
+        var activeIndex = findIndexOfDefaultElement(content, defaultElement);
         tabs[activeIndex].classList.add(activeTabClass);
     };
 
-    function findIndexOfDefaultElement(contentTabs, defaultContentTab){
+    function findIndexOfDefaultElement(contentTabs, defaultContentTab) {
         for (var i = 0; i < contentTabs.length; i++) {
             if (contentTabs[i] === defaultContentTab) {
                 return i;
             }
         }
-        throw new Error ("Could not find default in list");
+        throw new Error("Could not find default in list");
+    }
+
+    function checkOption(option, name) {
+        if (option === undefined) {
+            throw new Error("Expected " + name);
+        }
     }
 
 })();
