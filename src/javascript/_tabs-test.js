7b41b4a9cbb3afd00c9e7514d7933ef847add807
Gemma Waylett <gwaylett@gmail.com>
2019-11-15 13:41:12 -0500

    hes

--- src/javascript/_tabs-test.js
+++ src/javascript/_tabs-test.js
@@ -16,16 +16,22 @@
 
         afterEach(function () {
             removeElement(container);
-        })
+        });
 
-        it("hides multiple content elements except the default upon intitialization", function () {
+        it("hides all content elements except the default upon intitialization", function () {
+            var tab1 = addElement("div");
+            var defaultTab = addElement("div");
+            var tab3 = addElement("div");
+
             var element1 = addElement("div");
             var defaultElement = addElement("div");
             var element3 = addElement("div");
 
             tabs.initialize({
+                tabs: [tab1, defaultTab, tab3],
                 content: [element1, defaultElement, element3],
                 default: defaultElement,
+                activeTabClass: "activeTab",
                 contentHideClass: "hideClass"
             });
 
@@ -35,16 +41,40 @@
         });
 
         it("preserves existing classes when hiding an element.", function () {
+            var defaultTab = addElement("div");
+            var hiddenTab = addElement("div");
+
             var defaultElement = addElement("div");
             var hiddenElement = addElement("div");
             hiddenElement.setAttribute("class", "existingClass");
             tabs.initialize({
+                tabs: [defaultTab, hiddenTab],
                 content: [defaultElement, hiddenElement],
                 default: defaultElement,
+                activeTabClass: "activeTab",
                 contentHideClass: "newClass"
             });
 
             assert.equal(getClasses(hiddenElement), "existingClass newClass");
+        });
+
+        it("styles the default tab with a class", function(){
+            var defaultTab = addElement("div");
+            var defaultElement = addElement("div");
+
+            tabs.initialize({
+                tabs: [defaultTab],
+                content: [defaultElement],
+                default: defaultElement,
+                activeTabClass: "activeTab",
+                contentHideClass: "ignored"
+            });
+
+            assert.equal(getClasses(defaultTab), "activeTab");
+        });
+
+        it("preserves existing classes on the active tab", function(){
+            //TODO
         });
 
 
--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -2,12 +2,17 @@
     "use strict";
 
     exports.initialize = function initialize(options) {
+        var tabs = options.tabs;
         var content = options.content;
         var defaultElement = options.default;
+        var activeTabClass = options.activeTabClass;
         var contentHideClass = options.contentHideClass;
 
+
+        if (tabs === undefined) throw new Erorr ("Expected options.tab");
         if (content === undefined) throw new Error ("Expected options.content");
         if (defaultElement === undefined) throw new Error ("Expected options.default");
+        if (activeTabClass === undefined) throw new Error ("Expected options.activeTabClass");
         if (contentHideClass === undefined) throw new Error ("Expected options.contentHideClass");
 
         content.forEach(function(element) {
@@ -15,5 +20,9 @@
         });
 
         defaultElement.classList.remove(contentHideClass);
-    }
+
+        if (tabs !==  undefined) {
+            tabs[0].classList.add(activeTabClass);
+        }
+    };
 })();
