002d41402b6a08186aaaed88ae3e8fc45f372af4
Gemma Waylett <gwaylett@gmail.com>
2019-11-15 15:23:26 -0500

    hlsdf

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -14,17 +14,23 @@
         checkOption(activeTabClass, "options.activeTabClass");
         checkOption(hiddenContentClass, "options.hiddenContentClass");
 
-        var activeIndex = findIndexOfDefaultElement(tabs, defaultTab);
-        var defaultContent = content[activeIndex];
+        showTab(defaultTab, tabs, content, activeTabClass, hiddenContentClass);
 
+
+    };
+    
+
+    function showTab(tabToShow, tabs, content, activeTabClass, hiddenContentClass) {
+        var activeIndex = findIndexOfDefaultElement(tabs, tabToShow);
+        var defaultContent = content[activeIndex];
 
         content.forEach(function (element) {
             element.classList.add(hiddenContentClass);
         });
 
         defaultContent.classList.remove(hiddenContentClass);
-        defaultTab.classList.add(activeTabClass);
-    };
+        tabToShow.classList.add(activeTabClass);
+    }
 
     function findIndexOfDefaultElement(contentTabs, defaultContentTab) {
         for (var i = 0; i < contentTabs.length; i++) {
